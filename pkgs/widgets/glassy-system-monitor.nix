d56990b243b8b58715337e12cb19743f7402ce75
Muddyblack <70057554+Muddyblack@users.noreply.github.com>
2026-05-29 12:07:16 +0200

    udpaet glassy

--- pkgs/widgets/glassy-system-monitor.nix
+++ pkgs/widgets/glassy-system-monitor.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "glassy-system-monitor";
-  version = "0.0.11-beta";
+  version = "0.0.12-beta";
 
   src = fetchFromGitHub {
     owner = "Muddyblack";
     repo = "kde-glassy-system-monitor";
     rev = "refs/tags/v${version}";
-    hash = "sha256-HLTLIoFnFW0KueCy+ABvJIaGH+FelOQOYLCDCqRZYgI=";
+    hash = "sha256-V/mbbghIxGdqZHvs6TVqlIvwWuPtB/zyGOJ72uo95C0=";
   };
 
   dontConfigure = true;
