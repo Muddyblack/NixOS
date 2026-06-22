7fb50f70768d67385f7471baa648d5035fa3bfd5
Muddyblack <70057554+Muddyblack@users.noreply.github.com>
2026-06-22 16:58:57 +0200

    fix hyprland kde wallet prop

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -35,7 +35,7 @@
         "QT_QPA_PLATFORM,wayland;xcb"
         "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
         "NIXOS_OZONE_WL,1"
-        "XDG_CURRENT_DESKTOP,KDE:Hyprland"
+        "XDG_CURRENT_DESKTOP,Hyprland"
         "XDG_SESSION_TYPE,wayland"
         "XDG_SESSION_DESKTOP,Hyprland"
         "KDE_SESSION_VERSION,6"
