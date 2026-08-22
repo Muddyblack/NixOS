615270d2910ae77698db7022c0ea84f763393e17
Muddyblack <70057554+Muddyblack@users.noreply.github.com>
2026-08-22 23:17:59 +0200

    refactor: replace global desktop portal config with session-specific files to prevent portal lookup errors

--- modules/nixos/features/desktops.nix
+++ modules/nixos/features/desktops.nix
@@ -17,7 +17,7 @@
     };
     gnome.enable = lib.mkOption {
       type = lib.types.bool;
-      default = false;
+      default = true;
       description = "GNOME session";
     };
   };
--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,10 +1,13 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 lib.mkIf config.features.desktops.gnome.enable {
   services.desktopManager.gnome.enable = true;
 
   xdg.portal.config.gnome.default = ["gnome" "gtk"];
+
+  programs.ssh.askPassword = lib.mkForce "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
 }
